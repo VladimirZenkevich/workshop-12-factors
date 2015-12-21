c8410ef733df49ab0aa793ba052848591c035be0
uladzimir.ziankevich <AckiSkeiter1!>
2015-12-21 18:35:19 +0300

    Factor 3: Config.

--- src/main/java/com/altoros/stock/resources/StockResource.java
+++ src/main/java/com/altoros/stock/resources/StockResource.java
@@ -1,5 +1,7 @@
 package com.altoros.stock.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,10 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/stock", produces = MediaType.APPLICATION_JSON_VALUE)
 public class StockResource {
 
+    @Autowired
+    private Environment environment;
+
     @RequestMapping(value = "/ping", method = RequestMethod.GET)
     public String ping() {
-        return "Ping";
+        return "Ping" + " " + environment.getProperty("environment.variable");
     }
-
 }
 
--- src/test/java/com/altoros/stock/resources/StockResourceUnitTest.java
+++ src/test/java/com/altoros/stock/resources/StockResourceUnitTest.java
@@ -1,6 +1,12 @@
 package com.altoros.stock.resources;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.core.env.Environment;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -9,16 +15,24 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 /**
  * Created by uladzimir.ziankevich on 12/21/2015.
  */
+@RunWith(MockitoJUnitRunner.class)
 public class StockResourceUnitTest {
 
-    private MockMvc mockMvc = MockMvcBuilders.standaloneSetup(new StockResource()).build();
+    @Mock
+    private Environment environmentMock;
+    @InjectMocks
+    private StockResource stockResource = new StockResource();
+
+    private MockMvc mockMvc = MockMvcBuilders.standaloneSetup(stockResource).build();
 
     @Test
     public void testPing() throws Exception {
 
+        Mockito.when(environmentMock.getProperty("environment.variable")).thenReturn("test");
+
         mockMvc.perform(MockMvcRequestBuilders.get("/stock/ping"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().string("Ping"));
+                .andExpect(MockMvcResultMatchers.content().string("Ping test"));
 
     }
 }
